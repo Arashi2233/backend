a60f3a434fb2e95df616e7184fa25f9bc331e9cf
Arashi <Arashirasawa@outlook.com>
2025-05-16 19:51:28 +0800

    重构登录逻辑，使用缓存存储验证码并更新用户验证流程

--- UserApp/views/log_views.py
+++ UserApp/views/log_views.py
@@ -12,48 +12,65 @@ from utils.helper import check_code
 # 解决url中文乱码
 import urllib.parse
 from rest_framework.response import Response
+from base64 import b64encode
 
 from UserApp.models import UserList
-# Create your views here.
 
+from uuid import uuid4
+from django.core.cache import cache  # 默认内存，也可配置 Redis
+
+def store_code(code_str):
+    code_id = uuid4().hex
+    cache.set(code_id, code_str, timeout=60)  # 有效期60秒
+    print(code_str, code_id)
+    return code_id
+
+def get_code(code_id):
+    return cache.get(code_id)
+
+def delete_code(code_id):
+    cache.delete(code_id)
 
 @csrf_exempt
 def login(request):
     """ 用户登录 """
     user_data = JSONParser().parse(request)
-    # 判断验证码是否正确
-    image_code = request.session.get("image_code")
-    if image_code.upper() != user_data['code'].upper():
+    code_input = user_data.get("code")
+    code_id = user_data.get("code_id")
+
+    real_code = get_code(code_id)
+    if not real_code or real_code.upper() != code_input.upper():
         return JsonResponse({'code': -999, 'message': '验证码错误'}, safe=False)
+
+    delete_code(code_id)
 
     # 验证码正确，去数据库校验用户名和密码
     user = user_data['name']
-    pwd = user_data['pwd']
+    pwd = user_data['password']
+    role = user_data['role']
     encrypt_pasword = md5(pwd)
-    print(user, encrypt_pasword)
-    user_object = UserList.objects.filter(name=user, pwd=encrypt_pasword).first()
+    print( user, pwd, encrypt_pasword)
+    user_object = UserList.objects.filter(name=user, pwd=encrypt_pasword,role = role).first()
     if not user_object:
         return JsonResponse({'code': -999, 'message': '用户名或密码错误'}, safe=False)
 
-    request.session['info'] = {"id": user_object.id, 'name': user_object.name}
-    request.session.set_expiry(60 * 60 * 24 * 7)
-
-    return JsonResponse({'code': 200, 'message': '登录成功'}, safe=False)
+    return JsonResponse({'code': 200, 'message': '登录成功', 'user_id': user_object.id}, safe=False)
     
 @csrf_exempt
 def img_code(request):
     # 1.生成图片
     image_object, code_str = check_code()
+    code_id = store_code(code_str)
 
     # 2.图片内容返回写入内存，从内存读取并返回
     stream = BytesIO()
     image_object.save(stream, 'png')
+    base64_image = b64encode(stream.getvalue()).decode()
 
-    # 3.图片的内容写入session中 + 60s
-    request.session['image_code'] = code_str
-    request.session.set_expiry(60)
-
-    return HttpResponse(stream.getvalue(), content_type='image/png')
+    return JsonResponse({
+        "code_id": code_id,
+        "image": f"data:image/png;base64,{base64_image}"
+    })
 
 @csrf_exempt
 def logout(request):
